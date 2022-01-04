30ceabf5f73383d4f0b89eeac6f3d2b8223223d4
Abhishek Gupta <abhishek@abhishek.Gupta>
2022-01-04 17:57:56 +0530

    first commit

---
+++ src/io/oodles/LMDemo/Driver.java
@@ -0,0 +1,13 @@
+package io.oodles.LMDemo;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		p1.setName("Abhsihek");
+		p1.setAge(22);
+		p1.setSal(15000.0);
+		System.out.println(p1);
+	}
+
+}
---
+++ src/io/oodles/LMDemo/Person.java
@@ -0,0 +1,10 @@
+package io.oodles.LMDemo;
+
+import lombok.Data;
+
+public @Data class Person {
+	private String name;
+	private int age;
+	private double sal;
+}
+
